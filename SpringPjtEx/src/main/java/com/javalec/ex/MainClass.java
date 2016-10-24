10220c8d1aa1e4cf6a083dbea4f9020da84926b9
Jeong <Jeong@DESKTOP-HF8R5KF>
2016-10-24 16:12:19 +0900

    ㅜㅜ

---
+++ SpringPjtEx/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,14 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
+		Pencil pencil = ctx.getBean("pencil", Pencil.class);
+		pencil.use();
+	}
+
+}
---
+++ SpringPjtEx/src/main/java/com/javalec/ex/Pencil6BWithEraser.java
@@ -0,0 +1,10 @@
+package com.javalec.ex;
+
+public class Pencil6BWithEraser implements Pencil {
+
+	@Override
+	public void use() {
+		System.out.println("6B이고, 지우개가 있습니다.");
+	}
+
+}
